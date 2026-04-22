35e5655008ccd5b6edb4065ccdec134dabb9f0c6
neb636 <neb636@users.noreply.github.com>
2026-04-22 16:24:07 +0000

    deploy: 5437ac8fb1ebe49421fadba737cf7dccd0bed2d0

---
+++ pr-preview/pr-16/registerSW.js
@@ -0,0 +1,1 @@
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/robot-zombies/pr-preview/pr-16/sw.js', { scope: '/robot-zombies/pr-preview/pr-16/' })})}
